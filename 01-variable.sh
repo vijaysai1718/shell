659adc07152e652291592ca08032337a564ed211
vijaysai <vijaysai.chinthala45@gmail.com>
2025-02-13 13:42:09 +0530

    new changes to shell

---
+++ 01-variable.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# above command is shebang here # means commeting expect the first line # will behave as the commeting for the code if you use below 
+echo "scripting you have to execute"
+
+echo vijay, welcome to shell script
